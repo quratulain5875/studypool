e4b196bdc2a007c9de3668eebf70e6cdd97e6973
Qurat-Ul-Ain <quratulain5875@gmail.com>
2026-04-23 06:54:34 +0500

    change to save critera of changing and ss

--- studypool_clone/dbconnection.php
+++ studypool_clone/dbconnection.php
@@ -1,15 +1,10 @@
 <?php
-// Compatibility shim for case-sensitive filesystems (Linux containers).
-// The actual implementation lives in `dbconnection.php`.
-require_once __DIR__ . '/dbconnection.php';
-?>
-<?php
-// dbConnection.php
+// dbconnection.php
 // Uses environment variables (Docker) with fallback to local defaults (XAMPP)
 $servername = getenv('DB_HOST') ?: "localhost";
 $port = getenv('DB_PORT') ?: "3306";
 $username = getenv('DB_USER') ?: "root";
-$password = getenv('DB_PASSWORD') ?: "";
+$password = getenv('DB_PASSWORD') ?: "root_password";
 $dbname = getenv('DB_NAME') ?: "studypool_clone";
 
 $conn = new mysqli($servername, $username, $password, $dbname, (int)$port);
